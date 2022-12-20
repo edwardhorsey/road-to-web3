297d58b9d8118c129a105956a6b29a383f63bbef
Edward Horsey <edward.horsey@warnermusic.com>
2022-12-20 14:25:09 +0000

    Add deploy contract script. Use --network goerli flag to deploy

--- week-2/BuyMeACoffee-contracts/hardhat.config.ts
+++ week-2/BuyMeACoffee-contracts/hardhat.config.ts
@@ -1,8 +1,25 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { ethers } from "hardhat";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const GOERLI_URL = process.env.GOERLI_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+if (!GOERLI_URL || !PRIVATE_KEY) {
+    throw new Error("Please set environment variables");
+}
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+    solidity: "0.8.17",
+    networks: {
+        goerli: {
+            url: GOERLI_URL,
+            accounts: [PRIVATE_KEY],
+        },
+    },
 };
 
 export default config;
